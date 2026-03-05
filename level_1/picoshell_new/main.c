62406360b71807a102004dfbf5a484c85b883045
Javokhir Kubaev <jkubaev7@gmail.com>
2026-03-05 07:45:22 +0100

    Comments are in uzbek

--- level_1/picoshell_new/main.c
+++ level_1/picoshell_new/main.c
@@ -17,6 +17,15 @@ static int	count_commands(char	**cmds[])
 	return (i);
 }
 
+/*
+** picoshell funksiyasi buyruqni oladi va uni bajarilishini taminlaydi
+** num_cmds - qancha command borligini hisoblaydi
+** pfd - file descriptor uchun
+** pid - proccess id ni saqlash uchun
+** in_fd - oqish fayl deskriptorini saqlash uchun
+** status - 
+** ret - return valueni saqlash uchun
+*/
 static int	picoshell(char **cmds[])
 {
 	int	num_cmds;
@@ -30,51 +39,70 @@ static int	picoshell(char **cmds[])
 	in_fd = 0;
 	num_cmds = count_commands(cmds);
 	ret = 0;
+	// loop har bir command uchun
 	while(cmds[i])
 	{
+		// agar oxirgi kommand bolmasa pipe yaratish kerak
 		if (i < (num_cmds - 1))
-			pipe(pfd);
+			pipe(pfd);	// yangi pipe yaratish pfd[0] va pfd[1]
+		// yangi process yaratish
 		pid = fork();
+		// CHILD PROCESS :
 		if (pid == 0)
 		{
-			if (i < (num_cmds - 1))
+			// stdoutni pipega yonaltirish
+			if (i < (num_cmds - 1)) // agar oxirgi kommand bolmasa
 			{
-				dup2(pfd[1], 1);
-				close(pfd[0]);
-				close(pfd[1]);
+				dup2(pfd[1], 1); // stdout = pfd[1] (pipe write)
+				close(pfd[0]); // read end kerak emas
+				close(pfd[1]); // original write endni yop
 			}
-			if (i > 0)
+			// stdinni oldingi pipedan oqishga yonaltirish
+			if (i > 0) // agar birinchi command emas
 			{
-				dup2(in_fd, 0);
-				close(in_fd);
+				dup2(in_fd, 0); // stdin = in_fd (oldingi pipe read)
+				close(in_fd); // original in fayl deskriptorni yop
 			}
+			// commandi bajarish
 			execvp(cmds[i][0], cmds[i]);
 		}
-		if (i > 0)
-			close(in_fd);
-		if (i < (num_cmds - 1))
+		// PARENT PROCESS:
+
+		// Oldingi pipe read endini yopish
+		if (i > 0)	// Birinchi kommanddan keyin
+			close(in_fd); // Oldingi pipe read endni yop
+		// Keyingi iteratsiya uchun read endni saqlash
+		if (i < (num_cmds - 1)) // Oxirgi command emas
 		{
-			in_fd = pfd[0];
-			close(pfd[1]);
+			in_fd = pfd[0]; // Joriy pipe read endi = keyingi read end
+			close(pfd[1]); // write endni yop parent yozmaydi
 		}
 		i++;
 	}
-	while (wait(&status) > 0)
+	// Barcha child processlar tugashini kutish:
+	while (wait(&status) > 0) // har bir child tugaguncha
 	{
+		// Child normal exited va exit code != 0 bo'lsa:
 		if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
 			ret = 1;
+		// Child signal bilan tugagan bo'lsa:
 		else if (!WIFEXITED(status))
-			ret = 0;
+			ret = 1;
+		continue;
 	}
 	return (ret);
 }
 
+/*
+** Asosiy funksiya compile qilish uchun, u picoshell funksiyasini chaqiradi va 
+** buyruq bajarilishini nazorat qiladi
+*/
 int main(void)
 {
-	char	*cmd2[] = {"ls", "-l", NULL};
-	char	*cmd3[] = {"wc", "-l", NULL};
+	// char	*cmd2[] = {"ls", "-l", NULL};
+	// char	*cmd3[] = {"wc", "-l", NULL};
 	char	*cmd1[] = {"pwd", NULL};
-	char	**cmds[] = {cmd1, cmd2, cmd3, NULL};
+	char	**cmds[] = {cmd1, NULL};
 	int		ret;
 
 	ret = picoshell(cmds);
